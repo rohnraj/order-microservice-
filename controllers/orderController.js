4cb77ca6c1d88d131e86fc513a80f7895f74b2a9
Rohan Raj <rohanraj@Rohans-MacBook-Pro-2.local>
2026-04-02 16:27:34 +0530

    feat: integrate AMQP for order creation notifications

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -6,8 +6,12 @@ import {
 } from "../model/orderModel.js";
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
+import amqp from 'amqplib';
 
 dotenv.config();
+
+const connection = await amqp.connect('amqp://localhost');
+const channel = await connection.createChannel();
 
 const createOrderController = async (req, res) => {
     try {
@@ -20,7 +24,40 @@ const createOrderController = async (req, res) => {
         }
 
         const newOrder = await createOrder(id, items, total);
-        if(newOrder) {
+        if (newOrder) {
+
+            await channel.assertQueue("queue1", {
+                durable: true,
+                arguments: {
+                    'x-queue-type': 'quorum'
+                }
+            });
+
+            // channel.sendToQueue("queue1", Buffer.from("New order created: " + JSON.stringify(newOrder)), {
+            //     persistent: true
+            // });
+
+            // channel.sendToQueue("queue1", Buffer.from("User email: " + req.user.email), {
+            //     persistent: true
+            // });
+
+            channel.sendToQueue(
+                "queue1",
+                Buffer.from(JSON.stringify({
+                    event: "ORDER_CREATED",
+                    data: {
+                        order: JSON.stringify(newOrder),
+                        email: req.user.email
+                    }
+                })),
+                { persistent: true }
+            );
+
+            setTimeout(function () {
+                connection.close();
+                process.exit(0)
+            }, 500);
+
             res.status(201).json({ message: 'Order created successfully', order: newOrder });
         } else {
             res.status(400).json({ message: 'Failed to create order' });
@@ -32,7 +69,7 @@ const createOrderController = async (req, res) => {
 };
 
 const getAllOrdersController = async (req, res) => {
-    try{
+    try {
         const data = await getAllOrder();
         res.status(200).json({ orders: data });
     } catch (error) {
@@ -42,14 +79,14 @@ const getAllOrdersController = async (req, res) => {
 };
 
 const getOrderByIDController = async (req, res) => {
-    try{
+    try {
         const { id } = req.params;
         const data = await getOrderByID(id);
-        if(data) {
+        if (data) {
             res.status(200).json({ order: data });
         } else {
             res.status(404).json({ message: 'Order not found' });
-        }   
+        }
     } catch (error) {
         console.error('Error fetching order:', error);
         res.status(500).json({ message: 'Error fetching order' });
@@ -57,7 +94,7 @@ const getOrderByIDController = async (req, res) => {
 };
 
 const getUserOrdersController = async (req, res) => {
-    try{
+    try {
         const { id } = req.user || {};
         const data = await getUserOrders(id);
         res.status(200).json({ orders: data });
